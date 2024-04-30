28963bb50e23d63fca026e2498351e73311ff629
Vusal Osmanov <115807599+vusalosmanov@users.noreply.github.com>
2024-04-30 23:57:13 +0400

    add Login

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import Home from "../src/pages/home/Home";
 import Contact from "./pages/contact/Contact";
 import Header from "../src/components/header/Header"
 import Footer from "../src/components/footer/Footer"
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Reset from "./pages/auth/Reset";
+import OrderHistory from "./pages/orderHistory/OrderHistory";
 function App() {
   return (
     <BrowserRouter>
     <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
-        // <Route path="/contact" element={<Contact />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/order-history" element={<OrderHistory />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/reset" element={<Reset />} />
       </Routes>
       <Footer/>
     </BrowserRouter>
